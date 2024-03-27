606c1f578cdc1dee1b276722771359ea3692a064
Rahul Mistry <22cs042@charusat.edu.in>
2024-03-27 16:02:06 +0530

    Frontend Login

---
+++ Screens/SignupScreen.js
@@ -0,0 +1,7 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+import tw from "twrnc";
+
+export default function SignupScreen() {
+  return <SafeAreaView></SafeAreaView>;
+}
