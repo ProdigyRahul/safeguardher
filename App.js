c7c6fa6b84b665622fdfaa699714eb5aa4351107
Rahul Mistry <22cs042@charusat.edu.in>
2024-03-27 23:36:28 +0530

    Backend Frontend done with OTP

--- App.js
+++ App.js
@@ -4,36 +4,45 @@ import WelcomeScreen from "./Screens/WelcomeScreen";
 import LoginScreen from "./Screens/LoginScreen";
 import SignupScreen from "./Screens/SignupScreen";
 import HomeScreen from "./Screens/HomeScreen";
+import OTPVerifyScreen from "./Screens/OTPVerifyScreen";
 import { StatusBar } from "react-native";
+import { UserContext } from "./UserContext";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="dark-content" backgroundColor="#fdd9e4" />
-      <Stack.Navigator initialRouteName="Welcome">
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Welcome"
-          component={WelcomeScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={SignupScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <UserContext>
+      <NavigationContainer>
+        <StatusBar barStyle="dark-content" backgroundColor="#fdd9e4" />
+        <Stack.Navigator initialRouteName="Welcome">
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Welcome"
+            component={WelcomeScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Signup"
+            component={SignupScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="OTPVerify"
+            component={OTPVerifyScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </UserContext>
   );
 }
